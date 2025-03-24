ef42728cbb1e845dfbea22f56b2ec0813ecd63d9
Moozon <muzhenweizh@gmail.com>
2025-03-24 16:10:26 +0800

    feat: export json

---
+++ index.js
@@ -0,0 +1,30 @@
+// jsume schema
+import schema from './schemas/schema.json'
+// definitions
+import definitions from './schemas/definitions.json'
+// config schema
+import configSchema from './schemas/config.schema.json'
+
+// data schema
+import dataSchema from './schemas/data.schema.json'
+import aboutSchema from './schemas/data/about.schema.json'
+import awardsSchema from './schemas/data/awards.schema.json'
+import educationSchema from './schemas/data/education.schema.json'
+import languagesSchema from './schemas/data/languages.schema.json'
+import projectsSchema from './schemas/data/projects.schema.json'
+import skillsSchema from './schemas/data/skills.schema.json'
+import workSchema from './schemas/data/work.schema.json'
+
+export {
+  schema,
+  definitions,
+  configSchema,
+  dataSchema,
+  aboutSchema,
+  awardsSchema,
+  educationSchema,
+  languagesSchema,
+  projectsSchema,
+  skillsSchema,
+  workSchema,
+}
